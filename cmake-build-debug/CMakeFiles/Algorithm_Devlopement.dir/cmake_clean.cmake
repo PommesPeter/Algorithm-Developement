ea7c0edd72b64fbb2ec7724f75b3ed912c2c286e
PommesPeter <434596665@qq.com>
2021-02-05 15:05:16 +0800

    init repo

---
+++ cmake-build-debug/CMakeFiles/Algorithm_Devlopement.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Algorithm_Devlopement"
+  "Algorithm_Devlopement.pdb"
+  "CMakeFiles/Algorithm_Devlopement.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algorithm_Devlopement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
