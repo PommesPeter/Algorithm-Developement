cccea8f2483b41322febbd0ad64fa1fef42057bc
PommesPeter <434596665@qq.com>
2021-02-09 09:59:26 +0800

    update template code

--- Algorithm-Basic-Tutorial/ch1/bigInteger_add_template.cpp
+++ Algorithm-Basic-Tutorial/ch1/bigInteger_add_template.cpp
@@ -13,6 +13,7 @@ vector<int> add(vector<int> &A, vector<int> &B) {
     int t = 0;
 
     for (int i = 0; i < A.size() || i < B.size(); i++) {
+        //这里的核心思路就是逐位相加的时候要把每一位相加的结果的十位和个位分开，个位作为当前的位数，十位作为下一次计算的进位（也就是需要加到A[i+1]+B[i+1]）(这里不是累加，而是直接保存当前的进位)
         //每次算每一个位数都是三个数相加：A[i] + B[i] + 上一位的进位
         //不存在的位就看成0
         //这里就是用t来存A[i] + B[i]的结果
@@ -22,6 +23,7 @@ vector<int> add(vector<int> &A, vector<int> &B) {
         t /= 10;
     }
 
+    //加到最后一位时如果进位不是0那么就补1
     if (t != 0) C.push_back(1);
     return C;
 }
