3bd3678547a9fcd9d05f67f8988bb47e84336af7
PommesPeter <434596665@qq.com>
2021-02-14 01:01:37 +0800

    coding bigInteger div

--- Algorithm-Basic-Tutorial/ch1/bigInteger_div_template.cpp
+++ Algorithm-Basic-Tutorial/ch1/bigInteger_div_template.cpp
@@ -3,3 +3,36 @@
  * Data:2021-02-10
  * */
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> div(vector<int> &A, int &b) {
+
+    vector<int> C;
+    int t = 0;
+
+    for (int i = 0; i < A.size() || t != 0; i++) {
+        if (i < A.size()) t += (A[i] * b);
+        C.push_back(t % 10);
+        t /= 10;
+    }
+    while (C.size() > 1 && C.back() == 0)   C.pop_back();
+    return C;
+}
+
+int main() {
+
+    string a;
+    int b;
+    vector<int> A;
+    cin >> a >> b;
+    for (int i = a.length() - 1; i >= 0; i--) {
+        A.push_back(a[i] - '0');
+    }
+    auto C = mul(A, _b);
+    for (int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]);
+
+    return 0;
+}
